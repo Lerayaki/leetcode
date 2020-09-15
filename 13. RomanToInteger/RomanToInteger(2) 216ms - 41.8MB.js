87c4488770ab0ef5e1c30f1c4edb05ec6e49e115
Leralite <joshlera444@gmail.com>
2020-09-15 14:39:53 +0200

    Create RomanToInteger(2) 216ms - 41.8MB.js

---
+++ 13. RomanToInteger/RomanToInteger(2) 216ms - 41.8MB.js
@@ -0,0 +1,87 @@
+// Runtime: 264 ms, faster than 14.92% of JavaScript online submissions for Roman to Integer.
+// Memory Usage: 42.1 MB, less than 63.92% of JavaScript online submissions for Roman to Integer.
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var values = function(c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default: 
+                throw "Not valid";
+        }
+    }
+    var decimal = 0;
+    for (var i=s.length-1; i>=0; i--){
+        if (s[i] == 'I'){
+            decimal++;
+            continue;
+        }
+
+        if (s[i] == 'V'){
+            decimal += 5;
+            if (i > 0 && s[i-1] == 'I'){
+                decimal--;
+                i--;
+            }
+            continue;
+        }
+        if (s[i] == 'X'){
+            decimal += 10;
+            if (i > 0 && s[i-1] == 'I'){
+                decimal--;
+                i--;
+            }
+            continue;
+        }
+
+        if (s[i] == 'L'){
+            decimal += 50;
+            if (i > 0 && s[i-1] == 'X'){
+                decimal -= 10;
+                i--;
+            }
+            continue;
+        }
+        if (s[i] == 'C'){
+            decimal += 100;
+            if (i > 0 && s[i-1] == 'X'){
+                decimal -= 10;
+                i--;
+            }
+            continue;
+        }
+
+        if (s[i] == 'D'){
+            decimal += 500;
+            if (i > 0 && s[i-1] == 'C'){
+                decimal -= 100;
+                i--;
+            }
+            continue;
+        }
+        if (s[i] == 'M'){
+            decimal += 1000;
+            if (i > 0 && s[i-1] == 'C'){
+                decimal -= 100;
+                i--;
+            }
+            continue;
+        }
+    }
+    return decimal;
+};
