6cf84d3b202af188d4fc81793d9569c05c58f088
Leralite <joshlera444@gmail.com>
2020-09-19 01:20:42 +0200

    Create FindFirstAndLastPositionOfElementInSortedArray(2).cs & Update FindFirstAndLastPositionOfElementInSortedArray(1).cs

--- 34. FindFirstAndLastPositionOfElementInSortedArray/FindFirstAndLastPositionOfElementInSortedArray(1).cs
+++ 34. FindFirstAndLastPositionOfElementInSortedArray/FindFirstAndLastPositionOfElementInSortedArray(1).cs
@@ -2,30 +2,29 @@
 // Memory Usage: 32.3 MB, less than 5.09% of C# online submissions for Find First and Last Position of Element in Sorted Array.
 public class Solution {
     public int[] SearchRange(int[] nums, int target)
-        {
-            int[] output = { -1, -1 };
+    {
+        int[] output = { -1, -1 };
 
-            if (!nums.Contains(target))
-                return output;
-
-            int index = Find(nums, target);
-            output[0] = output[1] = index;
-
-            while (output[0] > 0 && nums[output[0]-1] == target)
-                output[0]--;
-
-            while (output[1] < nums.Length-1 && nums[output[1]+1] == target)
-                output[1]++;
-            
-
+        if (!nums.Contains(target))
             return output;
-        }
-        public int Find(int[] nums, int target)
-        {
-            if (nums[nums.Length / 2] == target)
-                return nums.Length / 2;
-            if (nums[nums.Length / 2] > target)
-                return Find(nums.Take(nums.Length / 2).ToArray(), target);
-            return nums.Length/2 + Find(nums.Skip(nums.Length / 2).ToArray(), target);
-        }
+
+        int index = Find(nums, target);
+        output[0] = output[1] = index;
+
+        while (output[0] > 0 && nums[output[0]-1] == target)
+            output[0]--;
+
+        while (output[1] < nums.Length-1 && nums[output[1]+1] == target)
+            output[1]++;
+        
+        return output;
+    }
+    public int Find(int[] nums, int target)
+    {
+        if (nums[nums.Length / 2] == target)
+            return nums.Length / 2;
+        if (nums[nums.Length / 2] > target)
+            return Find(nums.Take(nums.Length / 2).ToArray(), target);
+        return nums.Length/2 + Find(nums.Skip(nums.Length / 2).ToArray(), target);
+    }
 }
