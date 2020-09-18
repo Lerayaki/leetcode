6ff08de49428c975771a46a321c4e82c902d7bd8
Leralite <joshlera444@gmail.com>
2020-09-19 00:38:45 +0200

    Update LongestHappyString(1).cs

--- 1405. LongestHappyString/LongestHappyString(1).cs
+++ 1405. LongestHappyString/LongestHappyString(1).cs
@@ -1,52 +1,41 @@
+// Runtime: 76 ms, faster than 100.00% of C# online submissions for Longest Happy String.
+// Memory Usage: 22.8 MB, less than 47.37% of C# online submissions for Longest Happy String.
 public class Solution {
     public string LongestDiverseString(int a, int b, int c)
     {
         string s = string.Empty;
-
-        while (a > 0 || b > 0 || c > 0)
+        List<KeyValuePair<char, int>> list = new List<KeyValuePair<char, int>>()
         {
-            if (a > 0 && (s.Length == 0 || s[s.Length-1] != a))
+            new KeyValuePair<char, int>('a', a),
+            new KeyValuePair<char, int>('b', b),
+            new KeyValuePair<char, int>('c', c)
+        };
+
+        list.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+
+        char last = 'd';
+
+        while (true)
+        {
+            if (list[2].Key != last)
             {
-                for (int i=0; i<2; i++)
+                for (int i = 0; i < 2 && list[2].Value > 0; i++)
                 {
-                    s += 'a';
-                    a--;
-                    if (a == 0)
-                        break;
-                }
-                if (b == 0 && c == 0)
-                {
-                    break;
+                    s += list[2].Key;
+                    list[2] = new KeyValuePair<char, int>(list[2].Key, list[2].Value - 1);
+                    last = list[2].Key;
                 }
             }
-            if (b > 0 && (s.Length == 0 || s[s.Length - 1] != b))
+            else if (list[1].Value > 0)
             {
-                for (int i = 0; i < 2; i++)
-                {
-                    s += 'b';
-                    b--;
-                    if (b == 0)
-                        break;
-                }
-                if (a == 0 && c == 0)
-                {
-                    break;
-                }
+                s += list[1].Key;
+                list[1] = new KeyValuePair<char, int>(list[1].Key, list[1].Value - 1);
+                last = list[1].Key;
             }
-            if (c > 0 && (s.Length == 0 || s[s.Length - 1] != c))
-            {
-                for (int i = 0; i < 2; i++)
-                {
-                    s += 'c';
-                    c--;
-                    if (c == 0)
-                        break;
-                }
-                if (b == 0 && a == 0)
-                {
-                    break;
-                }
-            }
+            if (last == list[2].Key && list[1].Value+list[0].Value == 0)
+                break;
+
+            list.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
         }
 
         return s;
