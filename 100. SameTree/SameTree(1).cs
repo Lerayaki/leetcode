da09c60a3348d193419e03e9b3979e08d3ca6afc
Leralite <joshlera444@gmail.com>
2020-09-29 20:41:01 +0200

    Update SameTree(1).cs

--- 100. SameTree/SameTree(1).cs
+++ 100. SameTree/SameTree(1).cs
@@ -17,9 +17,7 @@ public class Solution {
     public bool IsSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null)
             return true;
-        if (p == null)
-            return false;
-        if (q == null)
+        if (p == null || q == null)
             return false;
         if (p.val != q.val)
             return false;
